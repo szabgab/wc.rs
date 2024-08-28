0c8733e817244b93d969d41bf2ed46200fb05f74
Gabor Szabo <gabor@szabgab.com>
2024-08-28 10:11:35 +0300

    create ws function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+pub fn wc(text: &str) -> usize {
+    text.len()
 }
 
 #[cfg(test)]
@@ -8,7 +9,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = wc("Hello world");
+        assert_eq!(result, 11);
     }
 }
