bcc0cfb37980603d5f9958ff6092ba699cdb08aa
Gabor Szabo <gabor@szabgab.com>
2024-08-28 10:51:25 +0300

    also handle number of newlines

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-pub fn wc_of_string(text: &str) -> (usize, usize) {
+pub fn wc_of_string(text: &str) -> (usize, usize, usize) {
     let bytes = text.len();
     let words = text.split_whitespace().collect::<Vec<_>>().len();
+    let lines = text.split('\n').collect::<Vec<_>>().len();
 
-    (bytes, words)
+    (bytes, words, lines - 1)
 }
 
-pub fn wc(filename: &str) -> (usize, usize) {
+pub fn wc(filename: &str) -> (usize, usize, usize) {
     let content = std::fs::read_to_string(filename).unwrap();
     wc_of_string(&content)
 }
@@ -19,17 +20,17 @@ mod tests {
         let filename = "files/hello.txt";
         let content = std::fs::read_to_string(filename).unwrap();
         let result = wc_of_string(&content);
-        assert_eq!(result, (11, 2));
+        assert_eq!(result, (11, 2, 0));
     }
 
     #[test]
     fn wc_works() {
         let cases = [
-            ("files/hello.txt", (11, 2)),
-            ("files/empty.txt", (0, 0)),
-            ("files/hello_with_newline.txt", (12, 2)),
-            ("files/spaces.txt", (47, 7)),
-            ("files/unicode.txt", (12, 2)),
+            ("files/hello.txt", (11, 2, 0)),
+            ("files/empty.txt", (0, 0, 0)),
+            ("files/hello_with_newline.txt", (12, 2, 1)),
+            ("files/spaces.txt", (47, 7, 3)),
+            ("files/unicode.txt", (12, 2, 1)),
         ];
 
         for (filename, bytes) in cases {
