7d3010df537a2590d4562c0550be59bae5192cec
Gabor Szabo <gabor@szabgab.com>
2024-08-28 10:43:13 +0300

    also handle number of words

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-pub fn wc_of_string(text: &str) -> usize {
-    text.len()
+pub fn wc_of_string(text: &str) -> (usize, usize) {
+    let words = text.split_whitespace().collect::<Vec<_>>().len();
+    (text.len(), words)
 }
 
-pub fn wc(filename: &str) -> usize {
+pub fn wc(filename: &str) -> (usize, usize) {
     let content = std::fs::read_to_string(filename).unwrap();
     wc_of_string(&content)
 }
@@ -16,17 +17,17 @@ mod tests {
         let filename = "files/hello.txt";
         let content = std::fs::read_to_string(filename).unwrap();
         let result = wc_of_string(&content);
-        assert_eq!(result, 11);
+        assert_eq!(result, (11, 2));
     }
 
     #[test]
     fn wc_works() {
         let cases = [
-            ("files/hello.txt", 11),
-            ("files/empty.txt", 0),
-            ("files/hello_with_newline.txt", 12),
-            ("files/spaces.txt", 47),
-            ("files/unicode.txt", 12),
+            ("files/hello.txt", (11, 2)),
+            ("files/empty.txt", (0, 0)),
+            ("files/hello_with_newline.txt", (12, 2)),
+            ("files/spaces.txt", (47, 7)),
+            ("files/unicode.txt", (12, 2)),
         ];
 
         for (filename, bytes) in cases {
